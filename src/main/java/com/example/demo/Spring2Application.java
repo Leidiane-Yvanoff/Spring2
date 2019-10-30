b76dbfb68ce431da0db12ba1884f98323eea2ad6
Leidiane YVANOFF <lydesign@MacBook-Pro-de-Leidiane.local>
2019-10-30 09:34:51 +0100

    Spring

--- src/main/java/com/example/demo/Spring2Application.java
+++ src/main/java/com/example/demo/Spring2Application.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
@@ -36,20 +37,23 @@ public class Spring2Application {
 	
     
 		 	
-			    @RequestMapping("/doctor/{id}")
-			    @ResponseBody
-			    public String docteurs(@PathVariable  int id) {
-			        
-			        
-			        if ( id < 0 || id > doctors.length ) 
-			        	throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + id);
-			             
-			        
-			        if(id <= 8) 
-			        	throw new ResponseStatusException(HttpStatus.SEE_OTHER, "page introuvable");
 
-			        return "Name : "  + doctors[id] + ", Number : "  + id ;         
-			        }
+    @RequestMapping("/doctor/{id}")
+    @ResponseBody
+    @ResponseStatus
+    public String docteurs(@PathVariable("id") int id) {
+        
+        int value = id - 1;
+        if ( value < 0 || value > doctors.length ) {
+        	throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation :" + id);
+        }     
+        
+        if(value < 8) {
+        	throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Cette page n'existe pas");
+        }
+        return ("{\"number\": "+ id  +", \"name\": \""+ doctors[value] +"\"}");
+        	        
+        }
 			 	
 	}
 
